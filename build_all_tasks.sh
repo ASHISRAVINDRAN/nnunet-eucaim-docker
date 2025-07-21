5c5114953d2fb5ad3dce64166c52d3f352b59713
Ashis Ravindran <ashis.ravindran@dkfz-heidelberg.de>
2025-07-21 13:07:58 +0200

    Minor updates

--- build_all_tasks.sh
+++ build_all_tasks.sh
@@ -3,10 +3,17 @@
 # List TASK_NAMES here
 TASK_NAMES=(
   Task002_Heart
+  Task004_Hippocampus
 )
+
+# User UID and Group ID defaults
+UID=${1:-1000}
+GID=${2:-1000}
 
 # Build base image first
 docker build \
+    --build-arg USER_UID=$UID\
+    --build-arg USER_GID=$GID\
     -t "nnunet:base" nnunet-base/
 echo "nnUNet base image build finshed"
 
@@ -16,7 +23,7 @@ for TASK_NAME in "${TASK_NAMES[@]}"; do
   
   docker build \
     --build-arg TASK_NAME=$TASK_NAME \
-    -t "nnunetv1:${TASK_NAME}" nnunet-task/
+    -t "nnunet:${TASK_NAME}" nnunet-task/
     
   echo "Building finshed for image: $TASK_NAME"
 done

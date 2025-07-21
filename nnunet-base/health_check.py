a009f8283ae563bdb0c57b7418ea4d10b5b65008
Ashis Ravindran <ashis.ravindran@dkfz-heidelberg.de>
2025-07-21 17:49:31 +0200

    Add HEALTHCHECK

---
+++ nnunet-base/health_check.py
@@ -0,0 +1,15 @@
+import os, sys
+
+def is_healthy():
+    try:
+        log_file = "/home/eucaim/nnUNet_output/plans.pkl"
+        if os.path.exists(log_file): # process started
+            return True
+    except Exception as e:
+        return False
+
+if __name__ == "__main__":
+    if is_healthy():
+        sys.exit(0)
+    else:
+        sys.exit(1)

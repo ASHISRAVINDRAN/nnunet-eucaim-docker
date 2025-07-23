1a74ac5d66305c86b019a3adedb691f2cdd1688f
Ashis Ravindran <ashis.ravindran@dkfz-heidelberg.de>
2025-07-23 23:09:50 +0200

    Add multi modal support

--- build_all_tasks.sh
+++ build_all_tasks.sh
@@ -2,9 +2,11 @@
 
 # List TASK_NAMES here
 TASK_NAMES=(
+  Task001_BrainTumour 
   Task002_Heart
   Task003_Liver
   Task004_Hippocampus
+  Task005_Prostate
   Task006_Lung
   Task007_Pancreas
   Task008_HepaticVessel
@@ -27,9 +29,17 @@ echo "nnUNet base image build finshed"
 for TASK_NAME in "${TASK_NAMES[@]}"; do
   echo "Building image for: $TASK_NAME"
   
-  docker build \
-    --build-arg TASK_NAME=$TASK_NAME \
-    -t "nnunet:${TASK_NAME}" nnunet-task/
-    
+  BUILD_CMD="docker build --build-arg TASK_NAME=$TASK_NAME"
+
+  case "$TASK_NAME" in
+    Task001_Brain|Task005_Prostate)
+    BUILD_CMD+=" --build-arg MULTI_MODAL=True"
+      ;;
+  esac
+
+  # Finalize and run the build command
+  BUILD_CMD+=" -t nnunet:${TASK_NAME} nnunet-task/"
+  eval $BUILD_CMD
+
   echo "Building finshed for image: $TASK_NAME"
 done
--- nnunet-base/start_nnunet.sh
+++ nnunet-base/start_nnunet.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# modify filename as nnUNet for mono-modal Nifti data
-for file in "$nnUNet_input"/*.nii.gz; do
-    filename=$(basename "$file")
-    if [[ "$filename" == *_0000.nii.gz ]]; then
-        continue
-    fi
-    base="${filename%.nii.gz}"
-    new_name="${base}_0000.nii.gz"
-    new_path="$nnUNet_input/$new_name"
+if [[ "$MULTI_MODAL" == "False" ]]; then
+    # modify filename as nnUNet for mono-modal Nifti data
+    for file in "$nnUNet_input"/*.nii.gz; do
+        filename=$(basename "$file")
+        if [[ "$filename" == *_0000.nii.gz ]]; then
+            continue
+        fi
+        base="${filename%.nii.gz}"
+        new_name="${base}_0000.nii.gz"
+        new_path="$nnUNet_input/$new_name"
 
-    mv "$file" "$new_path"
-    echo "Renamed $filename to $new_name"
-done
-
+        mv "$file" "$new_path"
+        echo "Renamed $filename to $new_name"
+    done
+fi
 nnUNet_predict -i $nnUNet_input -o $nnUNet_output -t $TASK_NAME -tr nnUNetTrainerV2 -p nnUNetPlansv2.1 -m 3d_fullres
